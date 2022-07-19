6b7c30d6fbf80379c2ca586bcafb37c13dd40771
X252458 <105844282+X252458@users.noreply.github.com>
2022-07-19 15:52:35 +0530

    Update TC08_Koodo_with_TAB_S_in_HPA.java

--- src/test/java/com/telus/rcms/tests/getEarlyRenewalPenalty/TC08_Koodo_with_TAB_S_in_HPA.java
+++ src/test/java/com/telus/rcms/tests/getEarlyRenewalPenalty/TC08_Koodo_with_TAB_S_in_HPA.java
@@ -101,8 +101,9 @@ public class TC08_Koodo_with_TAB_S_in_HPA extends BaseTest {
 		Reporting.setNewGroupName("GET REWARD SERVICE API CALL - AccessoryFinance");
 		String apiEnv = GenericUtils.getAPIEnvironment(environment);
 		Reporting.logReporter(Status.INFO, "API Test Env is : [" + apiEnv + "]");
-		// 8465602, 8465568, 8465520, 8466218
-		subscriptionID = "8465520";
+		// 8465602, 8465568, 8465520, 8466218 -> PT148
+		// 8386180, 8384504, 8386699  -> PT168
+		subscriptionID = "8386180";
 		Reporting.logReporter(Status.INFO, "Exisiting Subscription ID : " + subscriptionID);
 		startDate = JSONUtils.getGMTStartDate();
 		System.setProperty("karate.auth_token_violation", violationaccessToken);
